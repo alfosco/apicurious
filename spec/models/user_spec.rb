b3294d08f4858fd450a87cf57a0fa76a8b638ecc
Alex Fosco <alex.hf88@gmail.com>
2017-03-24 00:19:04 -0600

    adds simplecov

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+
+    before(:all) do
+      @user = User.new(uid: "1234567", username: "alfosco")
+    end
+
+    it "responds to uid" do
+      expect(@user).to respond_to(:uid)
+    end
+
+    it "responds to username" do
+      expect(@user).to respond_to(:username)
+    end
+  end
 end
--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,31 +1,24 @@
 require 'rails_helper'
 
-describe 'starred repos' do
-  it 'shows number of starred repositories' do
-    user = User.create(
-      uid: Figaro.env.github_client_id,
-      username: "alfosco",
-      token: ENV['TOKEN'],
-    )
-    stars = Starred.find_starred_repos(user)
-    star  = stars.first
+describe GithubService do
+  attr_reader :service
 
-    expect(stars.count).to eq(2)
-    expect(star[:name]).to eq('turing-rails_engine_api')
+  before(:each) do
+    current_user = User.create(uid: Figaro.env.github_client_id, username: "alfosco")
+    @service = GithubService.new(current_user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
   end
-end
 
-describe 'followers' do
-  it 'shows who is following you' do
-    user = User.create(
-      uid: Figaro.env.github_client_id,
-      username: "alfosco",
-      token: ENV['TOKEN'],
-    )
-    followers = Follower.find_followers(user)
-    follower = followers.first
+  describe 'starred repos' do
+    it 'returns starred repositories' do
+      VCR.use_cassette("services/starred") do
+        stars = @service.starred_repos
+        star  = stars.first
 
-    expect(followers.count).to eq(2)
-    expect(follower[:login]).to eq('janderson16')
+        expect(stars.count).to eq(2)
+        expect(star[:name]).to eq('turing-rails_engine_api')
+      end
+    end
   end
+
 end
